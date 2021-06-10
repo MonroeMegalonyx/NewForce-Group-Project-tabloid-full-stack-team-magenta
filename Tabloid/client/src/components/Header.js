22611931e8e230d0b70d5fba74973ccc6ac4bf69
Thomas Frazier <tfrazier2424@gmail.com>
2021-06-09 20:03:40 -0400

    made one more change to the tag navigation menu

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -51,7 +51,7 @@ export default function Header() {
 
                 <NavItem>
                   <NavLink tag={RRNavLink} to="/tags">
-                    Tag 
+                    Tags
                   </NavLink>
                 </NavItem>
 
