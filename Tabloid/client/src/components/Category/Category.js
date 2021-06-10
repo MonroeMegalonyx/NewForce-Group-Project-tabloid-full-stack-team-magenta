e4463dae06ed4919498d6f278feb5785fd816a08
Newforce-25 <mcspencesv@gmail.com>
2021-06-10 14:40:14 -0400

    working on edit feature

--- Tabloid/client/src/components/Category/Category.js
+++ Tabloid/client/src/components/Category/Category.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import { Card, CardImg, CardBody } from "reactstrap";
+import { CategoryContext } from "../../providers/CategoryProvider"
+import { useHistory } from "react-router-dom";
 
 export default function Category(category) {
+  const history = useHistory();
+  const {updateCategory} = useContext(CategoryContext)
+
+  const handleEdit = () => {
+      history.push("/category")
+    }
+  }
+
     return(
         <Card className="m-4">
       <p className="text-left px-2">{category.category.name}</p>
+      <button onClick={handleEdit}>Edit</button>
     </Card>
     );
 }
--- Tabloid/client/src/providers/CategoryProvider.js
+++ Tabloid/client/src/providers/CategoryProvider.js
@@ -23,7 +23,7 @@ export const CategoryProvider = (props) => {
                 .then(setCategory));
 
     const addCategory = (category) =>
-       getToken().then((token) =>
+        getToken().then((token) =>
             fetch("/api/category/", {
                 method: "POST",
                 headers: {
@@ -33,10 +33,21 @@ export const CategoryProvider = (props) => {
                 body: JSON.stringify(category)
             }));
 
+    const updateCategory = (categoryId) =>
+        getToken().then((token) =>
+            fetch(`/api/category/${categoryId}`, {
+                method: "PUT",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }, body: JSON.stringify(category)
+            }).then(getAllCategories));
+
+
 
 
     return (
-        <CategoryContext.Provider value={{ category, getAllCategories, addCategory }}>
+        <CategoryContext.Provider value={{ category, getAllCategories, addCategory, updateCategory }}>
             {props.children}
         </CategoryContext.Provider>
     );
