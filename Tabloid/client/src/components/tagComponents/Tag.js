9d4badad39bdd9679d9bde584ee2d7dbd45bb7cf
Thomas Frazier <tfrazier2424@gmail.com>
2021-06-10 13:38:15 -0400

    added delete method to the tag provider

--- Tabloid/client/src/components/tagComponents/Tag.js
+++ Tabloid/client/src/components/tagComponents/Tag.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardBody } from "reactstrap";
 
 export default function Tag({ tag }) {
-    // console.log(tag)
+    
   return (
     <Card className="m-4">
       <CardBody>
