ddcb50f1431edad9f1d627765a7ab0f92d6cec06
Newforce-25 <mcspencesv@gmail.com>
2021-06-10 11:43:11 -0400

    finished the delete category feature

--- Tabloid/client/src/components/Category/Category.js
+++ Tabloid/client/src/components/Category/Category.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import { Card, CardImg, CardBody } from "reactstrap";
+import { CategoryContext } from "../../providers/CategoryProvider"
+import { useHistory } from "react-router-dom";
 
 export default function Category(category) {
+  const history = useHistory();
+  const {deleteCategory} = useContext(CategoryContext)
+
+
+  const handleDelete = () => {
+    deleteCategory(category.category.id)
+    .then(() => {
+      history.push("/category")
+    })
+  }
+
     return(
         <Card className="m-4">
       <p className="text-left px-2">{category.category.name}</p>
+      <button onClick={handleDelete}>Delete</button>
     </Card>
     );
 }
